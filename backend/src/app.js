569dee570f7cdb5dcdb1833be62039dcc60e6f18
Madrigal-Carl <carlsalido.madrigal@gmail.com>
2026-06-20 11:17:59 +0800

    cors update

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,16 +9,9 @@ import taskRoutes from "./routes/task.routes.js";
 
 const app = express();
 
-const allowedOrigins = process.env.CLIENT_URL?.split(",") ?? [];
 app.use(
     cors({
-        origin: (origin, callback) => {
-            if (!origin || allowedOrigins.includes(origin)) {
-                callback(null, true);
-            } else {
-                callback(new Error("Not allowed by CORS"));
-            }
-        },
+        origin: process.env.CLIENT_URL,
         credentials: true,
     }),
 );
